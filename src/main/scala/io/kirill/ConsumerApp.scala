2265e3566e752fb456eb423d8e956dc3f82c1df1
kirill5k <immotional@aol.com>
2020-03-21 11:47:03 +0000

    kafka consumer examples

--- src/main/scala/io/kirill/ConsumerApp.scala
+++ src/main/scala/io/kirill/ConsumerApp.scala
@@ -6,35 +6,44 @@ import fs2.kafka._
 import scala.concurrent.duration._
 
 object ConsumerApp extends IOApp {
+
+  val consumerSettings =
+    ConsumerSettings(keyDeserializer = Deserializer[IO, String], valueDeserializer = Deserializer[IO, String])
+      .withAutoOffsetReset(AutoOffsetReset.Earliest)
+      .withBootstrapServers("localhost:29092")
+      .withGroupId("group")
+
+  def processRecord(record: ConsumerRecord[String, String]): IO[Unit] =
+    IO(println(s"Processing record: ${record.key} -> ${record.value}"))
+
   override def run(args: List[String]): IO[ExitCode] = {
-    def processRecord(record: ConsumerRecord[String, String]): IO[(String, String)] =
-      IO.pure(record.key -> record.value)
-
-    val consumerSettings =
-      ConsumerSettings[IO, String, String]
-        .withAutoOffsetReset(AutoOffsetReset.Earliest)
-        .withBootstrapServers("localhost:29092")
-        .withGroupId("group")
-
-    val producerSettings =
-      ProducerSettings[IO, String, String].withBootstrapServers("localhost:29092")
-
-    val stream =
-      consumerStream[IO]
-        .using(consumerSettings)
-        .evalTap(_.subscribeTo("topic"))
-        .flatMap(_.stream)
-        .mapAsync(25) { committable =>
-          processRecord(committable.record)
-            .map { case (key, value) =>
-              val record = ProducerRecord("topic", key, value)
-              ProducerRecords.one(record, committable.offset)
-            }
-        }
-        .through(produce(producerSettings))
-        .map(_.passthrough)
-        .through(commitBatchWithin(500, 15.seconds))
-
-    stream.compile.drain.as(ExitCode.Success)
+    simpleConsumer("topic.test").compile.drain.as(ExitCode.Success)
   }
+
+  def simpleConsumer(topic: String): fs2.Stream[IO, Unit] =
+    consumerStream(consumerSettings)
+      .evalTap(_.subscribeTo(topic))
+      .flatMap(_.stream)
+      .evalMap(commitable => processRecord(commitable.record))
+
+  /**
+   * Separates records per topic-partition
+   * The partitionStream in the example below is a Stream of records for a single topic-partition.
+   * Runs processRecord on every record, one-at-a-time in-order per topic-partition
+   */
+  def partitionedStream(topic: String): fs2.Stream[IO, Unit] =
+    consumerStream(consumerSettings)
+      .evalTap(_.subscribeTo(topic))
+      .flatMap(_.partitionedStream)
+      .map { partitionStream => partitionStream.evalMap(committable =>processRecord(committable.record)) }
+      .parJoinUnbounded
+
+  /**
+   * Use when processing of records is independent of each other
+   */
+  def asyncProcessingConsumer(topic: String): fs2.Stream[IO, Unit] =
+    consumerStream(consumerSettings)
+      .evalTap(_.subscribeTo(topic))
+      .flatMap(_.stream)
+      .mapAsync(25)(committable => processRecord(committable.record))
 }
---
+++ src/main/scala/io/kirill/ProducerApp.scala
@@ -0,0 +1,8 @@
+package io.kirill
+
+import cats.effect._
+
+object ProducerApp extends IOApp {
+
+  override def run(args: List[String]): IO[ExitCode] = ???
+}
