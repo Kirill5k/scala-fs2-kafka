648a54b65bb68d6751d0bd0ff7151176cd35b3cd
kirill5k <immotional@aol.com>
2020-03-27 10:11:21 +0000

    generic avro deserializer

--- src/main/scala/io/kirill/kafka/KafkaMessageConsumer.scala
+++ src/main/scala/io/kirill/kafka/KafkaMessageConsumer.scala
@@ -1,12 +1,15 @@
 package io.kirill.kafka
 
 import cats.effect._
-import fs2.kafka.{AutoOffsetReset, ConsumerRecord, ConsumerSettings, Deserializer, consumerStream}
-import io.kirill.configs.KafkaConsumerConfig
+import fs2.kafka.{AutoOffsetReset, ConsumerRecord, ConsumerSettings, Deserializer, KafkaDeserializer, consumerStream}
+import io.kirill.configs.{AppConfig, KafkaConsumerConfig}
 import io.kirill.event.Event
 import io.circe.generic.auto._
 import io.circe.parser._
+import io.confluent.kafka.serializers.KafkaAvroDeserializer
+import org.apache.avro.generic.GenericRecord
 
+import scala.jdk.CollectionConverters._
 
 class KafkaMessageConsumer[F[_]: Timer: ConcurrentEffect: ContextShift, K, V] private (
       config: KafkaConsumerConfig
@@ -38,6 +41,22 @@ object KafkaMessageConsumer {
   implicit def eventDeserializer[F[_]](implicit s: Sync[F]): Deserializer[F, Event] = Deserializer.instance {
     (_, _, bytes) => s.fromEither(decode[Event](bytes.map(_.toChar).mkString))
   }
+
+  implicit def genericAvroDeserializer[F[_]](implicit s: Sync[F], config: AppConfig): Deserializer[F, GenericRecord] =
+    Deserializer.delegate[F, GenericRecord](
+      new KafkaDeserializer[GenericRecord] {
+        private val avroDeserializer = new KafkaAvroDeserializer()
+        private val deserializerConf = Map(
+          "schema.registry.url" -> config.kafka.schema.schemaRegistryUrl,
+          "specific.avro.reader" -> true.asInstanceOf[java.lang.Boolean]
+        )
+        avroDeserializer.configure(deserializerConf.asJava, false)
+
+        override def deserialize(topic: String, data: Array[Byte]): GenericRecord = {
+          avroDeserializer.deserialize(topic, data).asInstanceOf[GenericRecord]
+        }
+      }
+    )
 
   def apply[F[_]: Timer: ConcurrentEffect: ContextShift, K, V](config: KafkaConsumerConfig)(implicit kd: Deserializer[F, K], vd: Deserializer[F, V]): KafkaMessageConsumer[F, K, V] =
     new KafkaMessageConsumer(config)
