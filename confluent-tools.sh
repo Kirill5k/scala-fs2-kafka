ffebc8d3b07133c4601377766467573fbe7c37e6
kirill5k <immotional@aol.com>
2020-03-27 09:58:13 +0000

    confluent tools for consuming avro

--- confluent-tools.sh
+++ confluent-tools.sh
@@ -2,4 +2,14 @@
 
 docker run -it --rm --net=host confluentinc/cp-schema-registry:3.1.1 bash
 
-kafka-avro-console-consumer
+kafka-avro-console-producer \
+    --broker-list localhost:29092 \
+    --topic test-avro \
+    --property schema.registry.url=http://127.0.0.1:8081 \
+    --property value.schema='{"type": "record", "name": "testRecord", "fields": [{"name": "foo", type: "string"}, {"name": "bar", type: "string"}]}'
+
+kafka-avro-console-consumer \
+    --bootstrap-server localhost:29092 \
+    --topic test-avro \
+    --property schema.registry.url=http://127.0.0.1:8081 \
+    --from-beginning
