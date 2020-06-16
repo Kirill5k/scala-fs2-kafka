bd00b7a66834c46adb65f9359fd5b1f9d9136960
kirill5k <immotional@aol.com>
2020-06-16 08:32:23 +0100

    filter template

--- src/main/scala/io/kirill/kafka/KafkaMessageStreams.scala
+++ src/main/scala/io/kirill/kafka/KafkaMessageStreams.scala
@@ -2,7 +2,7 @@ package io.kirill.kafka
 
 import java.util.Properties
 
-import cats.effect.Async
+import cats.effect.{Async, Sync}
 import cats.implicits._
 import org.apache.kafka.streams.KafkaStreams.State
 import org.apache.kafka.streams.{KafkaStreams, StreamsConfig}
@@ -26,14 +26,20 @@ final class KafkaMessageStreams[F[_]: Async](
     }
 }
 
-final class KafkaStreamsTopology[K, V](val builder: StreamsBuilder, val origin: KStream[K, V]) {}
+final class KafkaStreamsTopology[F[_]: Sync, K, V](
+    private val builder: StreamsBuilder,
+    private val origin: KStream[K, V]
+) {
+
+  def filter(p: (K, V) => F[Boolean]): KafkaStreamsTopology[F, K, V] = ???
+}
 
 object KafkaMessageStreams {
 
   def props: Properties = {
     val p = new Properties()
-    p.put(StreamsConfig.APPLICATION_ID_CONFIG, "wordcount-application")
-    p.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka-broker1:9092")
+    p.put(StreamsConfig.APPLICATION_ID_CONFIG, "kafka-streams-app")
+    p.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092")
     p
   }
 
