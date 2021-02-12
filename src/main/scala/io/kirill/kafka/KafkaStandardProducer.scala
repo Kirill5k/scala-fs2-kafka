f9df85268cf3a4775208249ae64559fcad85f6a0
kirill5k <immotional@aol.com>
2021-02-12 19:34:11 +0000

    refactor KafkaStandardProducer

--- src/main/scala/io/kirill/kafka/KafkaStandardProducer.scala
+++ src/main/scala/io/kirill/kafka/KafkaStandardProducer.scala
@@ -3,35 +3,37 @@ package io.kirill.kafka
 import java.util.Properties
 
 import cats.effect.{Async, Resource, Sync}
+import cats.implicits._
 import io.kirill.configs.KafkaConfig
 import org.apache.kafka.clients.producer.{Callback, KafkaProducer, ProducerConfig, ProducerRecord, RecordMetadata}
 import org.apache.kafka.common.serialization.Serializer
 
-class KafkaStandardProducer[F[_]: Async, K, V](
+final class KafkaStandardProducer[F[_], K, V](
     private val producer: KafkaProducer[K, V]
-) {
+)(implicit val F: Async[F]) {
 
-  def send(topic: String, key: K, value: V): F[Unit] = {
-    val record = new ProducerRecord[K, V](topic, key, value)
-    sendRecord(record)
-  }
+  def send(topic: String, key: K, value: V): F[Unit] =
+    F.delay(producer.send(new ProducerRecord[K, V](topic, key, value))).void
 
-  private def sendRecord(record: ProducerRecord[K, V]): F[Unit] =
+  def sendAsync(topic: String, key: K, value: V): F[Unit] =
     Async[F].async { k =>
-      val _ = producer.send(record, new Callback {
-        override def onCompletion(metadata: RecordMetadata, e: Exception): Unit =
-          if (metadata != null)
-            k(Right(()))
-          else
-            k(Left(e))
-      })
+      val _ = producer.send(
+        new ProducerRecord[K, V](topic, key, value),
+        new Callback {
+          override def onCompletion(metadata: RecordMetadata, e: Exception): Unit =
+            if (metadata != null)
+              k(Right(()))
+            else
+              k(Left(e))
+        }
+      )
     }
 }
 
 object KafkaStandardProducer {
 
-  def make[F[_]: Async, K, V](config: KafkaConfig)(
-      implicit kd: Serializer[K],
+  def make[F[_]: Async, K, V](config: KafkaConfig)(implicit
+      kd: Serializer[K],
       vd: Serializer[V]
   ): Resource[F, KafkaStandardProducer[F, K, V]] = {
     val acquireProducer = Sync[F].delay {
@@ -39,7 +41,7 @@ object KafkaStandardProducer {
       props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, config.servers)
       props.put(ProducerConfig.ACKS_CONFIG, "all")
       props.put(ProducerConfig.COMPRESSION_TYPE_CONFIG, "lz4")
-      new KafkaProducer[K, V](props)
+      new KafkaProducer[K, V](props, kd, vd)
     }
 
     Resource
